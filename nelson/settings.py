e891b20f0c2487dcdc67d97b13edaf1e9fa3740b
PeteNelson22 <peternelson063@gmail.com>
2023-01-05 22:07:18 +0100

    profile completed

--- nelson/settings.py
+++ nelson/settings.py
@@ -28,12 +28,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'project.apps.ProjectConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- project/models.py
+++ project/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 import uuid
+from users.models import Profile
+
 class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_img = models.ImageField(null=True, blank=True, default='default.jpg')
@@ -26,7 +29,7 @@ class Review(models.Model):
         ('down', ' Down Vote'),
     )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
-    # owner = 
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL) 
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
